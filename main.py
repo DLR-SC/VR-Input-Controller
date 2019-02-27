3c903ef7937745a71f503f60d0503cfa5645a4b8
Pseipel <Pseipel@web.de>
2019-02-27 15:52:12 +0100

    updated documentation

--- main.py
+++ main.py
@@ -14,24 +14,33 @@ help_text = "This is a sample controller for a system consisting of " \
             "is up and running. You can test this controller without setting any other services if you use the "
 
 
-# returns a help text and indicates that the application is running
 @app.route("/", strict_slashes=False)
 @app.route("/help/", strict_slashes=False)
 @app.route("/info/", strict_slashes=False)
 def info():
+    """
+    :return: a help text and indicates that the application is running.
+    """
     return help_text
 
 
-# response for unknown paths
 @app.route('/<path:path>')
 def catch_all(path):
+    """
+    response for unknown paths.
+    :param path: any path that is not declared in other functions.
+    :return: an error message.
+    """
     return 'You entered this path: %s. It might be, that either this path does not exist ' \
            'or you are using the GET method instead of POST or PUT' % path
 
 
-# processes the given data and returns a JSON containing the requested data
 @app.route("/api", methods=['POST', 'PUT'])
 def process_speech_input():
+    """
+    processes the given data and returns a JSON containing the requested data.
+    :return: a JSON containing the requested data from the database.
+    """
     application_state = request.json.get('application_state', None)
     user_utterance = request.json.get('user_utterance', None)
 
@@ -50,10 +59,13 @@ def process_speech_input():
     return build_response(speech_component_response)
 
 
-# This function returns all intents used by the speech processing service.
-# This information might be useful for developing a frontend
 @app.route("/api/intents", methods=['GET'])
 def get_all_intents():
+    """
+    This function returns all intents used by the speech processing service.
+    This information might be useful for developing a frontend.
+    :return: All intents used by the speech processing service.
+    """
     headers = {'Accept': 'application/json'}
     try:
         response = requests.get('http://localhost:5005/domain', headers=headers)
@@ -67,8 +79,11 @@ def get_all_intents():
         abort(404)
 
 
-# if given the context will be passed to the natural language component before the utterance is processed
 def set_speech_component_context(state):
+    """
+    If given, the context as state will be passed to the natural language component before the utterance is processed.
+    :param state: Application context.
+    """
     request_array = []
     for state_attribute in state:
         for key, value in state_attribute.iterItems():
@@ -82,14 +97,17 @@ def set_speech_component_context(state):
     response_slot_setting = requests.put('localhost:5005/conversations/' + sender_id + '/tracker/events', request_array)
 
     if response_slot_setting:
-        return response_slot_setting
+        print('successfully updated slots')
     else:
-        print('abort')
         abort(404)
 
 
-# the passes the users utterance to the speech component
 def request_speech_component_core(utterance):
+    """
+    Passes the users utterance to the speech component.
+    :param utterance: The users natural language utterance.
+    :return: The response from the nlp service.
+    """
     payload = '{"sender": "' + sender_id + '", "message": "' + utterance + '"}'
     headers = {'Content-type': 'application/json'}
     try:
@@ -102,8 +120,12 @@ def request_speech_component_core(utterance):
         abort(404)
 
 
-# builds response concerning out of the retrieved data
 def build_response(speech_component_response):
+    """
+    Builds response out of the retrieved data.
+    :param speech_component_response: The response made by the speech_component.
+    :return: A JSON with recipient_id, intent_name, natural_language_response, data and error.
+    """
     recipient_id = speech_component_response[0].get('recipient_id', None)
     if recipient_id is None:
         recipient_id = sender_id
@@ -120,6 +142,7 @@ def build_response(speech_component_response):
 
     speech_component_response_json = json.loads(speech_component_response[0]['text'])
     natural_language_response = 'here is what i found'
+    print(speech_component_response_json)
     intent_name = speech_component_response_json['intent']['name']
     confidence = speech_component_response_json['intent']['confidence']
     if confidence < 0.5:
@@ -147,8 +170,12 @@ def build_response(speech_component_response):
     })
 
 
-# simple parser, to extract the the needed information
 def convert_db_response(db_response):
+    """
+    A simple parser, to extract the the needed information from the database response.
+    :param db_response: The database response.
+    :return: The data field from the database response if not empty.
+    """
     if db_response:
         db_response = json.loads(db_response)
         if len(db_response['data']) == 0:
@@ -157,8 +184,12 @@ def convert_db_response(db_response):
     return ""
 
 
-# Queries the neo4j Database and returns as text
 def query_graph_db(graph_query):
+    """
+    Queries the neo4j Database and returns as text.
+    :param graph_query: The cypher query for the database.
+    :return: The database response.
+    """
     graph_query = graph_query.replace('"', '\\"')
 
     payload = '{"query": "' + graph_query + '"}'
@@ -173,9 +204,12 @@ def query_graph_db(graph_query):
         abort(404)
 
 
-# this is implemented for testing reasons
 @app.route("/test", methods=['POST', 'PUT'])
 def test_api():
+    """
+    This is implemented for testing reasons.
+    :return: Some test data for testing without the need to have a database or a nlp service.
+    """
     return jsonify({
         "recipient_id": 'default',
         "intent_name": 'showAllNodes',
@@ -212,6 +246,9 @@ def test_api():
     })
 
 
-# this method should return the data, for initial visualization if needed
 def initial_data():
-    return "allTheDataNeededForVisualisation"
+    """
+    In future, this method should return the data, for initial visualization if needed.
+    :return: an empty string
+    """
+    return ""
